b47dd48f139f32730a87bd1eda493bd7b0e3569b
nolanglenn <nolangglenn@gmail.com>
2019-01-28 00:20:08 -0600

    URL still broken

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,17 @@
-var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=eEA76kCOgvPo4ptrmjRG4e70iBRwN5TR&tag=cats";
+var userInput = $("#searchText").val().trim();
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-      var imageUrl = response.data.image_original_url;
-      var searchImage = $("<img>");
-      searchImage.attr("src", imageUrl);
-      searchImage.attr("alt", "search image");
-      $("#images").prepend(searchImage);
-});
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=eEA76kCOgvPo4ptrmjRG4e70iBRwN5TR&limit=10";
+
+$("#addButton").on('click', function() {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+          var imageUrl = response.data.image_original_url;
+          var searchImage = $("<img>");
+          searchImage.attr("src", imageUrl);
+          searchImage.attr("alt", "search image");
+          $("#images").prepend(searchImage);
+          console.log(userInput);
+    });
+})
